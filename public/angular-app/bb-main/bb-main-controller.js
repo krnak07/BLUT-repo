869eec8bdfcb5b9a7752ac208906fb5b6136e4a2
Kiran A K <krnak07@gmail.com>
2020-01-01 16:33:11 +0530

    validation

--- public/angular-app/bb-main/bb-main-controller.js
+++ public/angular-app/bb-main/bb-main-controller.js
@@ -1,18 +1,26 @@
 angular.module('bbApp')
 .controller('mainbbcontroller',mainbbcontroller);
 
-function mainbbcontroller($http,$routeParams) {
+function mainbbcontroller($http,$routeParams,$location) {
   var vm = this;
   var id = $routeParams.bankID;
-
-  $http.get('/api/bloodbank/'+id+'/test').then(function(response) {
-    vm.dat = response.data._id;
-    vm.name = response.data.name;
-    vm.phone = response.data.phoneNo;
-    vm.email = response.data.email;
-    vm.license = response.data.liscense;
-  })
-  .catch(function(error){
-    console.log(error);
-  });
+  if(!bb_id==''){
+    $http.get('/api/bloodbank/'+bb_id+'/test').then(function(response) {
+      vm.dat = response.data._id;
+      vm.name = response.data.name;
+      vm.phone = response.data.phoneNo;
+      vm.email = response.data.email;
+      vm.license = response.data.liscense;
+    })
+    .catch(function(error){
+      if(error.status == 404){
+        alert('login again');
+        $location.path('/bloodbank');
+      }
+    });
+  }
+  else{
+    alert('login again');
+    $location.path('/bloodbank');
+  }
 }
